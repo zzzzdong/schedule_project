395a66f42a18dbb08975383e10d4da30a4ade905
zzzzdong <jieun8496@icloud.com>
2025-05-19 10:26:03 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/schedule_project/ScheduleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.schedule_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/schedule_project/ScheduleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schedule_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
